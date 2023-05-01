81069dc6c958467599a97a307b2d94d9b26cbe25
sumanp <sumanpuri55@gmail.com>
2023-05-01 20:33:12 +0530

    Modeled schools

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_30_091350) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_01_145952) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "schools", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "established_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
