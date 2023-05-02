d16c9a5d8779afa14deac5f2edc5d44c7156033d
sumanp <sumanpuri55@gmail.com>
2023-05-02 20:35:59 +0530

    UI update & minor improvements

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -38,7 +38,7 @@ class EnrollmentsController < ApplicationController
   def update
     respond_to do |format|
       if @enrollment.update(enrollment_params)
-        format.html { redirect_to enrollment_url(@enrollment), notice: "Enrollment was successfully updated." }
+        format.html { redirect_to enrollments_url(), notice: "Enrollment was successfully updated." }
         format.json { render :show, status: :ok, location: @enrollment }
       else
         format.html { render :edit, status: :unprocessable_entity }
