0ad2ddd209da571a7c84f2ab1ff0d7f63851d1ec
sumanp <sumanpuri55@gmail.com>
2023-05-02 13:30:20 +0530

    Added basic authorization

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,4 +1,5 @@
 class SchoolsController < ApplicationController
+  load_and_authorize_resource
   before_action :set_school, only: %i[ show edit update destroy ]
 
   # GET /schools or /schools.json
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    
+    if user.super_admin?
+      can :manage, :all
+    elsif user.school_admin?
+      can :manage, School
+      can :manage, Course
+    else
+      can :read, :all
+    end
   end
 end
