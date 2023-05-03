c791c0785d918511a529d6fe93c238d34f842e55
sumanp <sumanpuri55@gmail.com>
2023-05-03 15:27:00 +0530

    minor fix

--- spec/controllers/schools_controller_spec.rb
+++ spec/controllers/schools_controller_spec.rb
@@ -2,15 +2,16 @@ require "rails_helper"
 require_relative "../support/devise"
 
 RSpec.describe SchoolsController, type: :controller do
-  describe "GET /schools" do
+  describe "GET index" do
     login_user
+    render_views
 
     context "from logged in user" do
       it "should return 200:OK" do
         school = FactoryBot.create(:school)
 
         get :index
-        expect(response).to have_http_status(:success)
+        expect(response).to be_successful
       end
     end
   end
